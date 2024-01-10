c0faffb639fc31a4ae97c692ba159a7067efcd8b
avinash <avinashtiwari2405@gmail.com>
2024-01-11 01:09:46 +0530

    teat the git

---
+++ ppattern.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "enter size" << endl;
+    cin >> n;
+    for (int i = 0; i <= n / 2; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j < n / 2 - i || j > n / 2 + i)
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+    for (int i = n / 2 + 1; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if ( j<n / 2 - (n - 1 - i) || j> n / 2 + (n - 1 - i))
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+}
